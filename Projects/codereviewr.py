05c2ae56735f98e81c4d3a0c557bff692b4e3950
atulpradhan2210 <picmisocialmedia@gmail.com>
2025-06-10 02:51:27 +0530

    Update codereviewr.py

--- Projects/codereviewr.py
+++ Projects/codereviewr.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import logging
